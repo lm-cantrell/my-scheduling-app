6eba0ecdc79334802988c23ccb542c9f09422c99
Lisa Cantrell <lcantr7@wgu.edu>
2022-02-22 17:35:42 -0700

    add db connection. add model classes. implement customer model

---
+++ src/model/Customer.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Customer {
+    private int customerId;
+    private String customerName;
+    private String address;
+    private String postalCode;
+    private String phoneNumber;
+    private int divisionId;
+
+    public Customer(int customerId, String customerName, String address, String postalCode, String phoneNumber, int divisionId) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phoneNumber = phoneNumber;
+        this.divisionId = divisionId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+
+}
