09c0466af52949fb260720b6105d485d896c458c
Lisa Cantrell <lcantr7@wgu.edu>
2022-02-23 16:17:24 -0700

    implement basic models for country division and contact

--- src/model/Division.java
+++ src/model/Division.java
@@ -1,4 +1,38 @@
 package model;
 
 public class Division {
+
+    private int divisionId;
+    private String divisionName;
+    private int assocCountryId;
+
+    public Division(int divisionId, String divisionName, int assocCountryId) {
+        this.divisionId = divisionId;
+        this.divisionName = divisionName;
+        this.assocCountryId = assocCountryId;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
+
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public int getAssocCountryId() {
+        return assocCountryId;
+    }
+
+    public void setAssocCountryId(int assocCountryId) {
+        this.assocCountryId = assocCountryId;
+    }
 }
